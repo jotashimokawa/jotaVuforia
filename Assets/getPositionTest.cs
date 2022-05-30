c10314e380068f4727cd46c0a37608ba120442ff
jotashimokawa <jotashimokawa@gmail.com>
2022-05-30 10:32:56 +0900

    とりあえずコミット

---
+++ Assets/getPositionTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getPositionTest : MonoBehaviour
+{
+    public GameObject imageTarget_vinland;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Transform trans_vin = imageTarget_vinland.GetComponent<Transform>();
+      //  Debug.Log(trans_vin.position);
+        
+    }
+}
