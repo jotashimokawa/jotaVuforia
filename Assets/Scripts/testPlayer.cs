4a1c18aa0c8cc4c89588960ab649484da4720e4c
jotashimokawa <jotashimokawa@gmail.com>
2022-06-13 15:07:58 +0900

    ゲームができるか試している．プレイヤーを仮で制作

---
+++ Assets/Scripts/testPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class testPlayer : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed = 1.0f;
+    Vector2 momentVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+    void OnMove(InputValue momentValue)
+    {
+        momentVector = momentValue.Get<Vector2>();
+        momentVector *= speed;
+
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.AddForce(momentVector.x, 0.0f, momentVector.y);
+
+    }
+}
